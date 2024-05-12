e3f5e02e528efd58cef66c44071a05b8d6198cff
Cumill11 <thekamkow@protonmail.com>
2024-05-12 15:55:14 +0200

    added words

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template, request
+import random
+from flask import Flask, render_template, request, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
-# Słowo, które gracz będzie próbował odgadnąć
-WORD = "PYTHON"
+app.secret_key = 'tajny_Sekretny_klucz_potrzebny_do_session'
+# Lista słów z tematyki przedmiotów szkolnych
+WORD_LIST = [
+    "MATEMATYKA",
+    "FIZYKA",
+    "CHEMIA",
+    "BIOLOGIA",
+    "INFORMATYKA",
+    "HISTORIA",
+    "GEOGRAFIA"
+    "WOS",
+    "PLASTYKA",
+    "MUZYKA"
+]
 
 # Maksymalna liczba błędnych prób (cały wisielec)
 MAX_WRONG_ATTEMPTS = 6
+
+# Wybieramy losowe słowo z listy WORD_LIST
+def choose_word():
+    return random.choice(WORD_LIST)
 
 # Lista liter, które gracz już wybrał
 guessed_letters = []
@@ -77,9 +93,10 @@ def generate_hangman_image(num_wrong):
 
 # Funkcja do sprawdzenia stanu gry (czy gracz wygrał, przegrał lub nadal gra)
 def check_game_state():
-    if all(letter in guessed_letters for letter in WORD):
+    word = session.get("word")
+    if all(letter in guessed_letters for letter in word):
         return "win"
-    incorrect_guesses = len([letter for letter in guessed_letters if letter not in WORD])
+    incorrect_guesses = len([letter for letter in guessed_letters if letter not in word])
     if incorrect_guesses >= MAX_WRONG_ATTEMPTS:
         return "lose"
     return "play"
@@ -93,19 +110,19 @@ def reset_game():
 @app.route("/", methods=["GET", "POST"])
 def home():
     global guessed_letters
-    
-    if request.method == "POST":
-        if request.form.get("action") == "new_game":
-            reset_game()
-        else:
-            letter = request.form["letter"].upper()
-            if letter not in guessed_letters:
-                guessed_letters.append(letter)
+    if request.method == "GET":
+        word = choose_word().upper()
+        session["word"] = word
+        reset_game()
+    elif request.method == "POST":
+        letter = request.form["letter"].upper()
+        if letter not in guessed_letters:
+            guessed_letters.append(letter)
 
     game_state = check_game_state()
-    hangman_image = generate_hangman_image(len([letter for letter in guessed_letters if letter not in WORD]))
+    hangman_image = generate_hangman_image(len([letter for letter in guessed_letters if letter not in session["word"]]))
 
-    return render_template("index.html", word=WORD, guessed_letters=guessed_letters,
+    return render_template("index.html", word=session["word"], guessed_letters=guessed_letters,
                            game_state=game_state, hangman_image=hangman_image)
 
 if __name__ == "__main__":
